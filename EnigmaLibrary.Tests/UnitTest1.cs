b2035d565a4dd0f8ae9fa80e48ac0fc0ffbb5b59
Patryk Moga <46868740+PatrykMoga@users.noreply.github.com>
2019-03-11 13:48:27 +0100

    commit

---
+++ EnigmaLibrary.Tests/UnitTest1.cs
@@ -0,0 +1,23 @@
+using NUnit.Framework;
+using EnigmaLibrary;
+using Autofac;
+
+namespace Tests
+{
+    public class Tests
+    {      
+        [Test]
+        public void Test1()
+        {
+            var container = Container.Configure();
+            using (var scope = container.BeginLifetimeScope())
+            {
+                var app = scope.Resolve<DecodingProcessor>();
+                var result = app.Decode("barbara");
+                Assert.That(result, Is.EqualTo("ADXCOTC"));
+            }
+            
+            
+        }
+    }
+}
